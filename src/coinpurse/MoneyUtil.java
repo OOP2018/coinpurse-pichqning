245e2310447dac792faf8eb68f61d4df9d3d0894
Pichaaun Popukdee <pichaaun.p@ku.th>
2018-01-24 22:20:06 +0700

    fix following latest issues

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -17,15 +17,38 @@ public class MoneyUtil {
 		java.util.Collections.sort( coins );
 		
 	}
+	
+	/**
+	 * This method is use for sort the coin which are the same currency.
+	 * @param coins (Contain value and currency of the coins).
+	 * @param currency (Currency that want to sort by).
+	 * @return the list of the coin that already sort by the currency.
+	 */
+	public static List<Coin> filterByCurrency (List<Coin> coins , String currency){
+		List<Coin> sortCurr = new ArrayList<Coin>();
+		for (Coin i : coins) {
+			if(i.getCurrency().toLowerCase().equals(currency.toLowerCase())) {
+				sortCurr.add(i);
+			}
+		}
+		return sortCurr ;
+	}
 	public static void main(String[] args) {
 		List<Coin> coins = new ArrayList<Coin>();
     	coins.add( new Coin (10.0,"Baht"));
     	coins.add( new Coin (0.5,"Baht"));
+    	coins.add( new Coin (20,"Dollar"));
+    	coins.add( new Coin (500,"Won"));
     	coins.add( new Coin (2.0,"Baht"));
+    	coins.add( new Coin (15,"Dollar"));
     	coins.add( new Coin (1.0,"Baht"));
+    	coins.add( new Coin (100,"Won"));
     	printCoins ( coins ) ;
     	sortCoins ( coins );
     	printCoins ( coins ) ;
+    	
+    	List<Coin> testFilbyCur = filterByCurrency(coins,"Baht");
+    	System.out.println("Sort by currency : " + testFilbyCur);
 	}
 	/**This method is the method for print all of coins.**/
 	 public static void printCoins (List<Coin> coins) {
